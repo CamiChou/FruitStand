f629a19bc2b5e9140d844da3c9776691996ccefc
= <heysamlee@gmail.com>
2021-07-14 23:07:23 -0700

    added new app files

---
+++ my-app/src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+
+import InstaPost from './components/InstaPost';
+
+import jungkook from './images/jungkook.png'
+import iuWithBun from './images/iu-bun.jpg';
+import iuCute from './images/iu-cute.png';
+import iuFunny from './images/iu-funny.jpg';
+
+const posts = [
+  {
+    accountName: 'eug.lo',
+    image: jungkook,
+    caption: 'wow stan jungkook',
+    altDescription: 'jungkook in a white hoodie'
+  },
+  {
+    accountName: 'jowody_lin',
+    image: iuWithBun,
+    caption: 'steamed buns!',
+    altDescription: 'IU holding a steamed bun'
+  },
+  {
+    accountName: 'not_jody8',
+    image: iuCute,
+    caption: 'why is she literally so perfect',
+    altDescription: 'IU in a green sweater'
+  },
+  {
+    accountName: 'defNotJody719',
+    image: iuFunny,
+    caption: '<3 <3 <3',
+    altDescription: 'IU making a funny face'
+  }
+]
+
+function App() {
+  return (
+    <div className="App">
+      {
+        posts.map(post => {
+          return <InstaPost
+            accountName={post.accountName}
+            image={post.image}
+            caption={post.caption}
+            altDescription={post.altDescription}
+            key={post.accountName}
+          />
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/InstaPost.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import './InstaPost.css';
+
+import heart from '../images/heart.png';
+import comment from '../images/comment.png';
+import share from '../images/share.png';
+
+function Comment(props) {
+  return <div>
+    <strong>eug.lo </strong>{props.text}
+  </div>
+}
+
+function Icon(props) {
+  return <img className='icon' alt={props.icon + " icon"} src={props.image} />
+}
+
+function InstaPost(props) {
+  
+  const [ likeCount, setLikeCount ] = useState(0);
+  const [ inputText, setInputText ] = useState('');
+  const [ comments, setComments ] = useState([]);
+
+  const incrementLikeCount = () => {
+    console.log("Incrementing like count!")
+    setLikeCount(likeCount + 1)
+  }
+
+  const updateInputBox = (event) => {
+    setInputText(event.target.value)
+  }
+
+  const addComment = () => {
+    if (inputText.trim() !== '') {
+      setComments([...comments, <Comment key={inputText} text={inputText}/>]);
+    }
+    setInputText('');
+  }
+
+  return <div className='post'>
+    <div className='account-line'>
+      <strong>{props.accountName}</strong>
+    </div>
+    <img 
+      className='post-img'
+      src={props.image}
+      alt={props.altDescription}
+      onClick={incrementLikeCount}
+    />
+    <div className='post-info'>
+      <div className='icons'>
+        <Icon icon='heart' image={heart} />
+        <Icon icon='comment' image={comment} />
+        <Icon icon='share' image={share} />
+      </div>
+      <div>
+        {likeCount} likes
+      </div>
+      <div>
+        <strong>{props.accountName}</strong> {props.caption}
+      </div>
+      <div className='comments'>
+        {comments}
+      </div>
+      <input type='text' value={inputText} onChange={updateInputBox}/>
+      <button onClick={addComment}>Comment</button>
+    </div>
+  </div>
+}
+
+export default InstaPost;
